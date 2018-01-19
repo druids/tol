b480fa9455cdc866be031d697cd89ee78bed3e5d
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-01-19 15:21:19 +0100

    Moved Clojure and ClojureScript dependencies into dev profile #24

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.946"]]
 
   :profiles {:dev {:plugins [[lein-cloverage "1.0.10"]
                              [lein-kibit "0.1.6"]
-                             [jonase/eastwood "0.2.5"]]}
+                             [jonase/eastwood "0.2.5"]]
+                   :dependencies [[org.clojure/clojure "1.9.0"]
+                                  [org.clojure/clojurescript "1.9.946"]]}
              :cljs {:plugins [[lein-cljsbuild "1.1.7"]
                               [lein-doo "0.1.8"]]
                     :doo {:build "test"}
